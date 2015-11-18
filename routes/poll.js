a7ea16d5b184a41b9b3e6b4264ec9e7cca43a2bb
Albert Fung <100520898@oncampus.local>
2015-11-18 18:30:12 -0500

    DONE BITCHES - except tests

--- routes/poll.js
+++ routes/poll.js
@@ -13,7 +13,8 @@ var database = require('../database');
  */
 router.post('/:poll_id/vote', auth.requireLevel('logged_in'), function (req, res, next) {
 
-    var poll_id = req.body.poll_id;
+    //var poll_id = req.body.poll_id;
+    var poll_id = req.params.poll_id;
     var userid = req.user.id;
     var answer = req.body.answer;
     
@@ -25,6 +26,7 @@ router.post('/:poll_id/vote', auth.requireLevel('logged_in'), function (req, res
     
     //res.send("$1, $2, $3", [poll_id, userid, answer]);
     
+
     database.query("INSERT INTO poll_votes (poll, user_id, answer) VALUES ($1, $2, $3)", [poll_id, userid, answer]).then(function () {
         return res.send({});
     }).catch(next);   
@@ -38,8 +40,13 @@ router.post('/:poll_id/vote', auth.requireLevel('logged_in'), function (req, res
  * Close the poll identified with :poll_id.
  */
 router.post('/:poll_id/close', auth.requireLevel('presenter'), function (req, res, next) {
-    var poll_id = req.params.poll_id;
-    res.send('Not yet implemented');
+    var poll_id = req.body.poll_id;
+   
+  
+    database.query("DELETE FROM polls WHERE id=($1)", [poll_id]).then(function () {
+        return res.send({});
+    }).catch(next);   
+    //res.send('Not yet implemented'); 
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var auth = require('../auth');
+var database = require('../database');
 
 /* albert was here 
  * GET /users/:user_id/
@@ -9,7 +10,12 @@ var auth = require('../auth');
  */
 router.get('/:user_id/', auth.requireLevel('logged_in'), function (req, res, next) {
     var user_id = req.params.user_id;
-    res.send('Not yet implemented');
+    
+    database.query("SELECT id, verified, presenter, email, password_hash, student_id FROM users WHERE id = $1", [user_id]).then(function (result) {
+        return res.send(result);
+    }).catch(next);
+    
+    //res.send('Not yet implemented');
 });
 
 module.exports = router;
