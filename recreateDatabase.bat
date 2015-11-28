c6fe7ce8420d9b8c8ae4259f6cb4a987f9411def
Patman64 <patman256@yahoo.com>
2015-11-27 22:25:08 -0500

    Add some users when creating database

--- recreateDatabase.bat
+++ recreateDatabase.bat
@@ -2,3 +2,4 @@
 echo DROP DATABASE IF EXISTS audience_connect; | psql -U postgres
 echo CREATE DATABASE audience_connect; | psql -U postgres
 psql -U postgres audience_connect < schema.sql
+psql -U postgres audience_connect < users.sql
---
+++ users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users VALUES (1, NULL, true, true, 'presenter@example.com', '$2a$04$ZcenQJERfUZowNUCgdoZ3eUVvNQOTE.aTjeHZzujGhsE1b55YT4pS', NULL, 'Presenter');
+INSERT INTO users VALUES (2, NULL, true, false, 'test@example.com', '$2a$04$TBjD9vaWYQxKwJTr8LWVp.ghfdCYRnUXMOxGeOV2c.JfQygRyx.TC', NULL, 'Tester');
